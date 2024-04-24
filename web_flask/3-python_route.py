5711bc628dcfdffcf20345247bf0e5d48b9b913c
jimmyMithamo <j1mithamo@gmail.com>
2024-04-24 14:24:33 +0300

    comments

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""script that returns 3 commands"""
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -6,22 +8,26 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def home():
+    """returns hello_hbnb"""
     return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
+    """returns HBNB"""
     return "HBNB"
 
 
 @app.route("/c/<text>", strict_slashes=False)
 def c_text(text):
+    """returns C and some text"""
     return "C {}".format(text.replace("_", " "))
 
 
 @app.route("/python/", defaults={'text': 'is_cool'}, strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def python_text(text):
+    """returns Python plus some text"""
     return "Python {}".format(text.replace("_", " "))
 
 
